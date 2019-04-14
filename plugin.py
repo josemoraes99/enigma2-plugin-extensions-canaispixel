a85551c8dc0f2876b40138f4032d1ec29ad83212
Jose Moraes <josemoraes@mbpretina.lan>
2019-04-14 17:17:00 -0300

    [update_14_04_2019]

--- plugin.py
+++ plugin.py
@@ -1,4 +1,7 @@
 ###########################################################################
+__version__             = "0.1.1"
+###########################################################################
+
 from Screens.Screen import Screen
 from Components.Label import Label
 from Components.ActionMap import ActionMap
@@ -33,6 +36,8 @@ __lambedbFile__         = __e2dir__ + 'lamedb5'
 __ignoreChannels__      = ['SID 0x']
 __localPiconDirectory__ = "/usr/share/enigma2/picon/"
 __urlPicons__           = "https://hk319yfwbl.execute-api.sa-east-1.amazonaws.com/prod"
+__updateurl__           = "https://raw.githubusercontent.com/josemoraes99/enigma2-plugin-extensions-canaispixel/master/plugin.py"
+__checkupdate__         = True
 
 ###########################################################################
 
@@ -192,6 +197,14 @@ class HalloWorldScreen(Screen):
 ###########################################################################
 def main(session, **kwargs):
     print ("\n[Hallo World] start\n")
+    if __checkupdate__:
+        print("check for update")
+        updateReturn = update(__updateurl__)
+        if updateReturn:
+            print("updating")
+            logging.info( "Reiniciando script" )
+            python = sys.executable
+            os.execl(python, python, *sys.argv)
     session.open(HalloWorldScreen)
 ###########################################################################
 def Plugins(**kwargs):
@@ -293,3 +306,143 @@ def iniciaDownloadPicons(cl):
     listFiles = gerarLista(channelList,lameDb,__ignoreChannels__)
 
     return listFiles
+
+###########################################################################
+
+def update(dl_url, force_update=False):
+    """
+Attempts to download the update url in order to find if an update is needed.
+If an update is needed, the current script is backed up and the update is
+saved in its place.
+"""
+    def compare_versions(vA, vB):
+        """
+Compares two version number strings
+@param vA: first version string to compare
+@param vB: second version string to compare
+@author <a href="http_stream://sebthom.de/136-comparing-version-numbers-in-jython-pytho/">Sebastian Thomschke</a>
+@return negative if vA < vB, zero if vA == vB, positive if vA > vB.
+"""
+        if vA == vB: return 0
+
+        def num(s):
+            if s.isdigit(): return int(s)
+            return s
+
+        seqA = map(num, re.findall('\d+|\w+', vA.replace('-SNAPSHOT', '')))
+        seqB = map(num, re.findall('\d+|\w+', vB.replace('-SNAPSHOT', '')))
+
+        # this is to ensure that 1.0 == 1.0.0 in cmp(..)
+        lenA, lenB = len(seqA), len(seqB)
+        for i in range(lenA, lenB): seqA += (0,)
+        for i in range(lenB, lenA): seqB += (0,)
+
+        rc = cmp(seqA, seqB)
+
+        if rc == 0:
+            if vA.endswith('-SNAPSHOT'): return -1
+            if vB.endswith('-SNAPSHOT'): return 1
+        return rc
+
+    # dl the first 256 bytes and parse it for version number
+    try:
+        http_stream = urllib.urlopen(dl_url)
+        # update_file = http_stream.read(256)
+        update_file = http_stream.read(300)
+        http_stream.close()
+
+    except IOError, (errno, strerror):
+        logging.info( "Unable to retrieve version data" )
+        logging.info( "Error %s: %s" % (errno, strerror) )
+        return
+
+    match_regex = re.search(r'__version__ *= *"(\S+)"', update_file)
+    if not match_regex:
+        logging.info( "No version info could be found" )
+        return
+    update_version = match_regex.group(1)
+
+    if not update_version:
+        logging.info( "Unable to parse version data" )
+        return
+
+    if force_update:
+        logging.info( "Forcing update, downloading version %s..." % update_version )
+
+    else:
+        cmp_result = compare_versions(__version__, update_version)
+        if cmp_result < 0:
+            logging.info( "Newer version %s available, downloading..." % update_version )
+        elif cmp_result > 0:
+            logging.info( "Local version %s newer then available %s, not updating." % (__version__, update_version) )
+            return
+        else:
+            logging.info( "You already have the latest version." )
+            return
+
+    # dl, backup, and save the updated script
+    # app_path = os.path.realpath(sys.argv[0])
+    # if __asModule__ == True:
+    app_path = __file__
+
+    if not os.access(app_path, os.W_OK):
+        logging.info( "Cannot update -- unable to write to %s" % app_path )
+
+    dl_path = app_path + ".new"
+    backup_path = app_path + ".old"
+    try:
+        dl_file = open(dl_path, 'w')
+        http_stream = urllib.urlopen(dl_url)
+        total_size = None
+        bytes_so_far = 0
+        chunk_size = 8192
+        try:
+            total_size = int(http_stream.info().getheader('Content-Length').strip())
+        except:
+            # The header is improper or missing Content-Length, just download
+            dl_file.write(http_stream.read())
+
+        while total_size:
+            chunk = http_stream.read(chunk_size)
+            dl_file.write(chunk)
+            bytes_so_far += len(chunk)
+
+            if not chunk:
+                break
+
+            percent = float(bytes_so_far) / total_size
+            percent = round(percent*100, 2)
+            sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" %
+                (bytes_so_far, total_size, percent))
+
+            if bytes_so_far >= total_size:
+                sys.stdout.write('\n')
+
+        http_stream.close()
+        dl_file.close()
+    except IOError, (errno, strerror):
+        logging.info( "Download failed" )
+        logging.info( "Error %s: %s" % (errno, strerror) )
+        return
+
+    try:
+        os.rename(app_path, backup_path)
+    except OSError, (errno, strerror):
+        logging.info( "Unable to rename %s to %s: (%d) %s" % (app_path, backup_path, errno, strerror) )
+        return
+
+    try:
+        os.rename(dl_path, app_path)
+    except OSError, (errno, strerror):
+        logging.info( "Unable to rename %s to %s: (%d) %s" % (dl_path, app_path, errno, strerror) )
+        return
+
+    try:
+        import shutil
+        shutil.copymode(backup_path, app_path)
+    except:
+        os.chmod(app_path, 0755)
+
+    logging.info( "New version installed as %s" % app_path )
+    logging.info( "(previous version backed up to %s)" % (backup_path) )
+    return True
